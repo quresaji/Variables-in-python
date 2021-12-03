52a7565842bcba961f1498a63b3c3a6423cbfc58
quresaji <sajidqureshi5254@gmail.com>
2021-12-04 03:06:04 +0530

    Varibale examples

--- Variable.py
+++ Variable.py
@@ -19,3 +19,38 @@ my_name="Sajid"
 
 print(type(my_name)) # we are checking the data type of variable
 print(type(_s))
+
+# It is difficult to write the variable names in more than one word so there are following ways to do that
+# 1. Camel case
+#except first letter all letter should be in capital
+
+myVariableName=" Harry"
+
+print(myVariableName)
+
+# 2. Pascal Case
+
+# All first letters will be in capital
+
+MyVariableName="Danny"
+
+print(MyVariableName)
+
+# Snake case
+# each word will be separated by and underscore
+
+my_name=" Jack"
+
+print(my_name)
+print(type("MyVariableName"))
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
---
+++ string.py
@@ -0,0 +1,9 @@
+# string interpolation/substitution
+
+w1= 'Sajid Qureshi'
+
+w2= "Python"
+ # My name is Sajid Qureshi , I am learning python.
+
+msg=" My name is {}, I am learning {}."
+print(msg.format(w1,w2))
