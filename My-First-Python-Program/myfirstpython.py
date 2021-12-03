e27551f1750b133af1ee1b740e17002573bf6e89
quresaji <sajidqureshi5254@gmail.com>
2021-12-04 02:18:34 +0530

    Today I am writing about variables

---
+++ My-First-Python-Program/myfirstpython.py
@@ -0,0 +1,1 @@
+print(" Sajid is a good guy")
---
+++ Variable.py
@@ -0,0 +1,21 @@
+# I am going to declare variables in python
+x=5
+y=10
+print(x+y)
+
+print(type(x))
+
+y="Sajid"
+print(type(y))
+"""
+A variable name must start with a letter or the underscore character
+"""
+_s=20
+_y=40
+print(_s+_y)
+
+# A variable cannot start by number 
+my_name="Sajid"
+
+print(type(my_name)) # we are checking the data type of variable
+print(type(_s))
