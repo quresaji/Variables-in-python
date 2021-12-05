f2c0b44d57c5f5d61e55dcf00b0a74977fc64949
quresaji <sajidqureshi5254@gmail.com>
2021-12-05 17:35:51 +0530

    List

---
+++ List.py
@@ -0,0 +1,25 @@
+# List
+
+# Lists are used to store multiple items in a single variable
+#List are created using square brackets
+# In Python List duplicate entities are allowed
+
+List=['Sunil','Ravi', "vicky"]
+
+print(List)
+# Index number will start from 0'
+
+print(List[2])
+
+# We can store multiple datatypes in one list
+
+msg=[" Ajay", 5,"Sajid",2,"Dipesh"]
+
+print(msg)
+print(msg[2])
+
+# string interpolation
+# Ajay Sajid Dipesh are students of MCA 1st sem
+print(msg[0],msg[2],msg[4],'are students of MCA 1st sem')
+
+
