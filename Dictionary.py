00ef3f81a257ecda7b5679f13f0c1e7955a89d3f
quresaji <sajidqureshi5254@gmail.com>
2021-12-05 22:54:55 +0530

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,36 @@
+# I am creqating a python dictionary 
+
+Marx={
+#P:V ( where P stands for property and V stands for value)
+"Brand":"Tata",
+
+"Model":"Nexon",
+
+"Year":"2021"
+
+}
+
+print(Marx)
+
+#dictName [PropertyName]
+
+print(" I have a "+ Marx["Brand"]+'  '+Marx["Model"]+"Car")
+
+print(f" I have a {Marx['Brand']}  {Marx['Model']} Car ")
+
+
+
+Friend={
+
+'Name':'Ajay ',
+
+"Age":"22"
+}
+
+#Now I want to change the age
+
+Friend['Age']="23"
+
+print(Friend)
+
+
---
+++ Tuple.py
@@ -0,0 +1,17 @@
+# Tuples
+
+# A tuple is data type which is used to store multiple items in single variable
+
+# A tuple is a collection which is ordered and unchangeable
+
+Teacher=("Pandey Sir","Gaur Sir","Anil Dollar","Asha Mam")
+
+print(Teacher)
+
+print(Teacher[3])
+
+
+
+Class=("Ajay, Abhishek,Deepak, Dipesh")
+
+print(Class, " are  top 4 students of the class")
